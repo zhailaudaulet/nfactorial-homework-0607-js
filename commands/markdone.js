998348a304b785d42c4dab95fa5eda7c89a2038a
Daulet <daulet.ilchanov@gmail.com>
2022-06-10 11:12:35 +0600

    add hw

---
+++ commands/markdone.js
@@ -0,0 +1,33 @@
+const conf = new (require('conf'))()
+
+
+function markDone({tasks}) {
+    let todosList = conf.get('todo-list')
+
+    if (todosList) {
+        //loop over the todo list tasks
+        todosList = todosList.map((task, index) => {
+            //check if the user specified the tasks to mark done
+            if (tasks) {
+                //check if this task is one of the tasks the user specified
+                if (tasks.indexOf(index.toString()) !== -1) {
+                    //mark only specified tasks by user as done
+                    task.done = true
+                }
+            } else {
+                //if the user didn't specify tasks, mark all as done
+                task.done = true
+            }
+            return task
+        });
+
+        //set the new todo-list
+        conf.set('todo-list', todosList)
+    }
+
+    //show the user a message
+    console.log('Tasks have been marked as done successfully')
+
+
+}
+module.exports = markDone
